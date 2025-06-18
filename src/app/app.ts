664a7ede586860c1310390eae0a50a2d39bd55f4
Carlos170702 <108914307+Carlos170702@users.noreply.github.com>
2025-06-18 08:35:50 -0700

    scheduler app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeEs);
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }, // Configura el locale a español
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
